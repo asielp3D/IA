1949fcfb5abfeea3cbe720021ccbcf0b3d0482be
asielp3D <asielmasespacio@gmail.com>
2024-01-26 11:29:49 +0100

    sasas

--- Assets/Scripts/IAEnemy.cs
+++ Assets/Scripts/IAEnemy.cs
@@ -22,6 +22,7 @@ public class IAEnemy : MonoBehaviour
     [SerializeField] Vector2 patrolAreaSize;
 
     [SerializeField] float visionRange = 15;
+    [SerializeField] float visionAngle = 90;
 
     void Awake()
     {
@@ -66,6 +67,11 @@ public class IAEnemy : MonoBehaviour
     void Chase()
     {
         enemyAgent.destination = playerTransform.position;
+
+        if(OnRange() == false)
+        {
+            currentState = State.Patrolling;
+        }
     }
 
     void SetRabdomPoint()
@@ -79,12 +85,21 @@ public class IAEnemy : MonoBehaviour
 
     bool OnRange()
     {
-        if(Vector3.Distance(transform.position, playerTransform.position)<= visionRange)
+        /*if(Vector3.Distance(transform.position, playerTransform.position)<= visionRange)
         {
             return true;
         }
 
-        return false;
+        return false;*/
+
+        Vector3 directionToPlayer = playerTransform.position - transform.position;
+        float distanceToPlayer = directionToPlayer.magnitude;
+        float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+        
+        if(distanceToPlayer <= visionRange && angleToPlayer < visionAngle * 0.5f)
+        {
+
+        }
     }
 
     void OnDrawGizmos()
@@ -95,5 +110,10 @@ public class IAEnemy : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, visionRange);
 
+        Gizmos.color = Color.green;
+        Vector3 fovLine1 = Quaternion.AnlgeAxis(visionAngle *0,5f, transform.up) * transform.forward * visionRange;
+        Vector3 fovLine2 = Quaternion.AnlgeAxis(-visionAngle *0,5f, transform.up) * transform.forward * visionRange;
+        Gizmos.DrawLine (transform.position, transform.position + fovLine1);
+        Gizmos.DrawLine (transform.position, transform.position + fovLine2);
     }
 }
